130c307f8b0e307d444e7acb02dbc95741952cc5
yameneslam215-ship-it <yameneslam215@gmail.com>
2026-09-16 20:09:26 +0300

    Create Parser.h

---
+++ Bank_System/Parser.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <vector>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+class Parser {
+public:
+    static vector<string> split(string line);
+    static Client parseToClient(string line);
+    static Employee parseToEmployee(string line);
+    static Admin parseToAdmin(string line);
+};
+
+inline vector<string> Parser::split(string line) {
+    stringstream ss(line);
+    string t;
+    vector<string> r;
+    while (getline(ss, t, ',')) r.push_back(t);
+    return r;
+}
+
+inline Client Parser::parseToClient(string line) {
+    auto v = split(line);
+    return Client(v[0], v[1], v[2], stod(v[3]));
+}
+
+inline Employee Parser::parseToEmployee(string line) {
+    auto v = split(line);
+    return Employee(v[0], v[1], v[2], stod(v[3]));
+}
+
+inline Admin Parser::parseToAdmin(string line) {
+    auto v = split(line);
+    return Admin(v[0], v[1], v[2], stod(v[3]));
+}
